895643b972291c3f8f4dc90afbeb3675bddbc250
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-05-24 19:49:07 +0400

    Save changes to User.js before switching branches

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Loader } from "./Loader";
-import { PostListItem } from "./PostListItem";
+import { PostListItem } from "./PostListItem.js";
 import { useLocation } from "react-router-dom";
 
 export const User = () => {
